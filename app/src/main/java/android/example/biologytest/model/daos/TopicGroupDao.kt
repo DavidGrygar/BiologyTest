0b5fd1781e26ac0c3bbad2d529dd31e290de9ea5
DavidGrygar <davidgrygar@protonmail.com>
2020-12-22 11:12:12 +0100

    Create TopicGroupDao.kt

---
+++ app/src/main/java/android/example/biologytest/model/daos/TopicGroupDao.kt
@@ -0,0 +1,9 @@
+package android.example.biologytest.model.daos
+
+import android.example.biologytest.model.entities.TopicGroupEntity
+import androidx.room.Dao
+
+@Dao
+interface TopicGroupDao: BaseDao<TopicGroupEntity> {
+    
+}
