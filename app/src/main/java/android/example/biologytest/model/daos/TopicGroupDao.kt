706676b3dd11e2fdc1a722c41aeba9ce1d768829
DavidGrygar <davidgrygar@protonmail.com>
2020-12-22 11:45:31 +0100

    TopicGroup Repository and Dao methods added with flow return

--- app/src/main/java/android/example/biologytest/model/daos/TopicGroupDao.kt
+++ app/src/main/java/android/example/biologytest/model/daos/TopicGroupDao.kt
@@ -1,9 +1,17 @@
 package android.example.biologytest.model.daos
 
+import android.example.biologytest.model.entities.AnswerEntity
 import android.example.biologytest.model.entities.TopicGroupEntity
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TopicGroupDao: BaseDao<TopicGroupEntity> {
-    
+    @Query("SELECT * FROM TOPIC_GROUP WHERE ID = :id LIMIT 1")
+    fun getSingle(id: Long): Flow<TopicGroupEntity>
+
+    @Query("SELECT * FROM TOPIC_GROUP")
+    fun getList(): Flow<List<TopicGroupEntity>>
 }
--- app/src/main/java/android/example/biologytest/repository/TopicGroupRepository.kt
+++ app/src/main/java/android/example/biologytest/repository/TopicGroupRepository.kt
@@ -1,10 +1,12 @@
 package android.example.biologytest.repository
 
 import android.example.biologytest.model.daos.TopicGroupDao
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 
 class TopicGroupRepository @Inject constructor(
     private val topicGroupDao: TopicGroupDao
 ) {
-
+    @ExperimentalCoroutinesApi
+    fun getAllTopicGroups() = topicGroupDao.getList()
 }
